3fe22959b850920eb9872989b531524e9d09f006
osbrown1 <socknot@live.com>
2023-03-13 13:30:46 -0700

    update

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -2,10 +2,10 @@ using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
-    public class Category
-    {
-        public int CategoryId { get; set; }
-        public string Name { get; set; }
-        public List<Item> Items { get; set; }
-    }
+  public class Category
+  {
+    public int CategoryId { get; set; }
+    public string Name { get; set; }
+    public List<Item> Items { get; set; }
+  }
 }
